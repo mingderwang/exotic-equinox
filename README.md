994e918acf6b2d614b8ed4dcfd98e34a69455b6c
Ming-der Wang <mingderwang@gmail.com>
2023-10-22 13:02:23 +0800

    add vercel adaptor for astro

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # features
 
+    * add vercel adaptor for astro
     * add radix.UI with tailwindCSS
     * add icons from @astropub/icons, see /icons
     * create -> npm create astro@latest -- --template withastro/astro/examples/middleware
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import react from "@astrojs/react";
-
+import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel(),
+  output: 'server',
   integrations: [react(), tailwind()],
   vite: {
     ssr: {
